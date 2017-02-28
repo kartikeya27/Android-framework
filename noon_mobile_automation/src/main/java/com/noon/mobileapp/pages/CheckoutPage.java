5c05622096393faf651baa6e9057c7425f21242f
aipopa <apopa@noon.com>
2017-02-28 12:12:38 +0400

    checkout verify test

--- noon_mobile_automation/src/main/java/com/noon/mobileapp/pages/CheckoutPage.java
+++ noon_mobile_automation/src/main/java/com/noon/mobileapp/pages/CheckoutPage.java
@@ -33,194 +33,252 @@ public class CheckoutPage extends BasePage {
 		
 	}
 
-//	Comment CHECKOUT PAGE
+    String SELECT_DELIVERY_MSG = "//*[contains(@text,'Select your delivery method')]";
+    String DEFAULT_ADDRESS = "//*[contains(@text,'Default')]";
+    String PAYMENT_PAY_WITH_MSG = "//*[contains(@text,'Pay with')]";
+    String PAY_ON_DELIVERY = "//*[contains(@text,'Pay on delivery')]";
+    String PLACE_ORDER_BTN = "//*[contains(@text,\"Place Order\")]";
+    String SUBTOTAL = "//*[contains(@resource-id,\"com.noon:id/checkout_subtotal_value\")]";
+    String GRAND_TOTAL = "//*[contains(@resource-id,\"com.noon:id/checkout_grandtotal\")]";
+
+
+    @FindBy(xpath = NConstants.SELECT_DELIVERY_MSG)
+    public AndroidElement deliveryMsg;
+
+    @FindBy(xpath = NConstants.DEFAULT_ADDRESS)
+    public AndroidElement defaultAddress;
+
+    @FindBy(xpath = NConstants.PAYMENT_PAY_WITH_MSG)
+    public AndroidElement paymentMsg;
+
+    @FindBy(xpath = NConstants.PAY_ON_DELIVERY)
+    public AndroidElement payOnDeliveryOption;
+
+    @FindBy(xpath = NConstants.PLACE_ORDER_BTN)
+    public AndroidElement placeOrderBtn;
+
+    @FindBy(xpath = NConstants.SUBTOTAL)
+    public AndroidElement subtotal;
+
+    @FindBy(xpath = NConstants.GRAND_TOTAL)
+    public AndroidElement grandTotal;
+
+    public void checkoutVerify(){
+
+        WebDriverWait wait = new WebDriverWait(aDriver, 20);
+
+        scrollToElement(NConstants.SELECT_DELIVERY_MSG,"down");
+        wait = new WebDriverWait(aDriver, 20);
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(NConstants.DEFAULT_ADDRESS)));
+        defaultAddress.click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(NConstants.PAYMENT_PAY_WITH_MSG)));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(NConstants.PAY_ON_DELIVERY)));
+        payOnDeliveryOption.click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(NConstants.SUBTOTAL)));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(NConstants.GRAND_TOTAL)));
+
+
+    }
+
+
+
+
+
+
+
+
+	public void otherStuff(){
+		//	Comment CHECKOUT PAGE
 //    TO FIX
 
     /*
 
 	@FindBy(xpath=NConstants.SIGNIN_CENTRAL_BUTTON)
 	public AndroidElement signinCentalButton;
-	
+
 	@FindBy(xpath=NConstants.SIGN_IN_TAB)
 	public AndroidElement signinTab;
-	
+
 	@FindBy(xpath=NConstants.SIGNIN_EMAIL)
 	public AndroidElement signinEmail;
-	
+
 	@FindBy(xpath=NConstants.SIGNIN_PASSWORD)
 	public AndroidElement signinPassword;
-	
+
 	@FindBy(xpath=NConstants.SIGNIN_BUTTON)
 	public AndroidElement signinButton;
-	
+
 	@FindBy(xpath=NConstants.MY_ACCOUNT)
 	public AndroidElement myAccount;
-	
+
 	@FindBy(xpath=NConstants.NO_ORDERS)
 	public AndroidElement noOrders;
-	
+
 	@FindBy(xpath=NConstants.SEARCH_TEXT_VIEW)
 	public AndroidElement searchTextView;
-	
+
 	@FindBy(xpath=NConstants.SEARCH_IMAGEVIEW)
 	public AndroidElement searchImageview;
-	
+
 	@FindBy(xpath=NConstants.SEARCH_BAR_CONTAINER)
 	public AndroidElement searchBarContainer;
-	
+
 	@FindBy(xpath=NConstants.PRODUCT_TITILE)
 	public AndroidElement productTitle;
-	
+
 	@FindBy(xpath=NConstants.PRODUCT_PRICE)
 	public AndroidElement productPrice;
-	
+
 	@FindBy(xpath=NConstants.PRODUCT_CART)
 	public AndroidElement productCart;
-	
+
 	@FindBy(xpath=NConstants.PRODUCT_MENU_CART)
 	public AndroidElement productMenuCart;
-	
+
 	@FindBy(xpath=NConstants.CART_SINGLE_LABEL)
 	public AndroidElement cartSingleLabel;
-	
+
 	@FindBy(xpath=NConstants.CART_ITEM_DELETE)
 	public AndroidElement cartItemDelete;
-	
+
 	@FindBy(xpath=NConstants.CART_ITEM_DECREASE)
 	public AndroidElement cartItemDecrease;
-	
+
 	@FindBy(xpath=NConstants.CART_ITEM_INCREASE)
 	public AndroidElement cartItemIncrease;
-	
+
 	@FindBy(xpath=NConstants.CART_CHECKOUT_BUTTON)
 	public AndroidElement cartCheckoutButton;
-	
+
 	@FindBy(xpath=NConstants.PICK_LOCATION)
 	public AndroidElement pickLocation;
-	
+
 	@FindBy(xpath=NConstants.CHECKOUT_ADDRESS)
 	public AndroidElement checkoutAddress;
-	
+
 	@FindBy(xpath=NConstants.CHECKOUT_ADDRESS1)
 	public AndroidElement checkoutAddress1;
-	
+
 	@FindBy(xpath=NConstants.CHECKOUT_ADDRESS2)
 	public AndroidElement checkoutAddress2;
-	
+
 	@FindBy(xpath=NConstants.CHECKOUT_ADDRESS_TYPE)
 	public AndroidElement checkoutAddressType;
-	
+
 	@FindBy(xpath=NConstants.CHECKOUT_ADDRESS_NAME)
 	public AndroidElement checkoutAddressName;
-	
+
 	@FindBy(xpath=NConstants.CHECKOUT_ADDRESS_SELECT)
 	public AndroidElement checkoutAddressSelect;
-	
+
 	@FindBy(xpath=NConstants.CHECKOUT_PERSON_NAME)
 	public AndroidElement checkoutPersonName;
-	
+
 	@FindBy(xpath=NConstants.CHECKOUT_PERSON_MOBILE)
 	public AndroidElement checkoutPersonMobile;
-	
+
 	@FindBy(xpath=NConstants.CHECKOUT_PERSON_APARTMENT)
 	public AndroidElement checkoutPersonApartment;
-	
+
 	@FindBy(xpath=NConstants.CHECKOUT_PERSON_FLOOR)
 	public AndroidElement checkoutPersonFloor;
-	
+
 	@FindBy(xpath=NConstants.CHECKOUT_PERSON_BUILDING)
 	public AndroidElement checkoutPersonBuilding;
-	
+
 	@FindBy(xpath=NConstants.CHECKOUT_PERSON_AREA_NAME)
 	public AndroidElement checkoutPersonAreaName;
-	
+
 	@FindBy(xpath=NConstants.CHECKOUT_PERSON_CITY)
 	public AndroidElement checkoutPersonCity;
-	
+
 	@FindBy(xpath=NConstants.CHECKOUT_PERSON_LANDMARK)
 	public AndroidElement checkoutPersonLandmark;
-	
+
 	@FindBy(xpath=NConstants.CHECKOUT_PERSON_COUNTRY)
 	public AndroidElement checkoutPersonCountry;
-	
+
 	@FindBy(xpath=NConstants.CHECKOUT_SAVE_ADDRESS)
 	public AndroidElement checkoutSaveAddress;
-	
+
 	@FindBy(xpath=NConstants.SHIP_CHOOSE_ADDRESS)
 	public AndroidElement shipChooseAddress;
-	
+
 	@FindBy(xpath=NConstants.SHIP_ADDRESS_ALERT_CANCEL)
 	public AndroidElement shipAddressAlertCancel;
-	
+
 	@FindBy(xpath=NConstants.SHIP_INFO)
 	public AndroidElement Shipinfo;
-	
+
 	@FindBy(xpath=NConstants.SHIP_INFO_IMAGE)
 	public AndroidElement shipInfoImage;
-	
+
 	@FindBy(xpath=NConstants.SHIP_CONTINUE_BUTTON)
 	public AndroidElement shipContinueButton;
-	
+
 	@FindBy(xpath=NConstants.PAYMENT_CARD_NUMBER)
 	public AndroidElement paymentCardNumber;
-	
+
 	@FindBy(xpath=NConstants.PAYMENT_CARD_MONTH)
 	public AndroidElement paymentCardMonth;
-	
+
 	@FindBy(xpath=NConstants.PAYMENT_CARD_YEAR)
 	public AndroidElement paymentCardYear;
-	
+
 	@FindBy(xpath=NConstants.PAYMENT_CVV_CODE_NUMBER)
 	public AndroidElement paymentCvvCodeNumber;
-	
+
 	@FindBy(xpath=NConstants.PAYMENT_CVV_CODE_INFO)
 	public AndroidElement PaymentCvvCodeInfo;
-	
+
 	@FindBy(xpath=NConstants.PAYMENT_CVV_CODE_INFO_IMAGE)
 	public AndroidElement PaymentCvvCodeInfoImage;
-	
+
 	@FindBy(xpath=NConstants.PAYMENT_CARD_SAVE)
 	public AndroidElement paymentCardSave;
-	
+
 	@FindBy(xpath=NConstants.PAY_BUTTON)
 	public AndroidElement payButton;
-	
-	
-	
-	
-	
+
+
+
+
+
 	public void checkout(String userName, String userPassword) {
-		
+
 		WebDriverWait wait = new WebDriverWait(aDriver, 20);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(NConstants.SIGNIN_CENTRAL_BUTTON)));
 		signinCentalButton.click();
-		
+
 		wait = new WebDriverWait(aDriver, 20);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(NConstants.SIGN_IN_TAB)));
 		signinTab.click();
-		
+
 		signinEmail.sendKeys(userName);
 		aDriver.hideKeyboard();
-		
+
 		signinPassword.sendKeys(userPassword);
 		aDriver.hideKeyboard();
-		
+
 		signinButton.click();
-		
+
 		//wait = new WebDriverWait(aDriver, 20);
 		//wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(NConstants.SEARCH_IMAGEVIEW)));
 		//searchImageview.sendKeys(Keys.RETURN);
 		//aDriver.pressKeyCode(66);
 		//wait = new WebDriverWait(aDriver, 20);
 		//wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(NConstants.MY_ACCOUNT)));
-		
+
 		//Assert.assertTrue(isElementPresent(NConstants.MY_ACCOUNT), "Could not find my account link");
 		//myAccount.click();
-		
+
 		//wait = new WebDriverWait(aDriver, 20);
 		//wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(NConstants.NO_ORDERS)));
 		//Assert.assertTrue(isElementPresent(NConstants.NO_ORDERS), "Could not find no orders show");
-		
+
 		wait = new WebDriverWait(aDriver, 20);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(NConstants.SEARCH_TEXT_VIEW)));
 		Assert.assertTrue(isElementPresent(NConstants.SEARCH_TEXT_VIEW), "Could not find search text area");
@@ -234,61 +292,61 @@ public class CheckoutPage extends BasePage {
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(NConstants.SEARCH_BAR_CONTAINER)));
 		searchBarContainer.sendKeys("TheBalm Bronzer/Blush");
 		//aDriver.hideKeyboard();
-		
+
 		//searchTextView.sendKeys(Keys.RETURN);
 		wait = new WebDriverWait(aDriver, 20);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(NConstants.LIST_ITEM_LABEL)));
 		Assert.assertTrue(isElementPresent(NConstants.LIST_ITEM_LABEL), "Could not select search product label"+NConstants.LIST_ITEM_LABEL);
 		aDriver.pressKeyCode(66); // This is virtual keyboard enter key value
-		
+
 	    //InputMethodManager imm = (InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
-		
+
 		wait = new WebDriverWait(aDriver, 20);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(NConstants.PRODUCT_IMAGE)));
 		Assert.assertTrue(isElementPresent(NConstants.PRODUCT_RESULT), "Could not load product result"+NConstants.PRODUCT_RESULT);
 		Assert.assertTrue(isElementPresent(NConstants.PRODUCT_IMAGE), "Could not load product image"+NConstants.PRODUCT_IMAGE);
 		Assert.assertTrue(isElementPresent(NConstants.PRODUCT_TITILE), "Could not load product title"+NConstants.PRODUCT_TITILE);
 		Assert.assertTrue(isElementPresent(NConstants.PRODUCT_PRICE), "Could not load product price"+NConstants.PRODUCT_PRICE);
-		
+
 		Assert.assertEquals(productTitle.getText(), "Bahama Mama Bronzer");
 		Assert.assertEquals(productPrice.getText(), "AED 104");
-		
+
 		wait = new WebDriverWait(aDriver, 20);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(NConstants.PRODUCT_CART)));
 		productCart.click();
-		
+
 		wait = new WebDriverWait(aDriver, 20);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(NConstants.PRODUCT_MENU_CART)));
 		productMenuCart.click();
-		
+
 		wait = new WebDriverWait(aDriver, 20);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(NConstants.CART_SINGLE_LABEL)));
-		
+
 		Assert.assertTrue(isElementPresent(NConstants.CART_SINGLE_LABEL), "Could not load item label"+NConstants.CART_SINGLE_LABEL);
 		Assert.assertTrue(isElementPresent(NConstants.CART_ITEM_DELETE), "Could not load delete button "+NConstants.CART_ITEM_DELETE);
 		Assert.assertTrue(isElementPresent(NConstants.CART_ITEM_DECREASE), "Could not load decrease image"+NConstants.CART_ITEM_DECREASE);
 		Assert.assertTrue(isElementPresent(NConstants.CART_ITEM_INCREASE), "Could not load increase image"+NConstants.CART_ITEM_INCREASE);
-		
+
 		wait = new WebDriverWait(aDriver, 20);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(NConstants.CART_CHECKOUT_BUTTON)));
 		Assert.assertTrue(isElementPresent(NConstants.CART_CHECKOUT_BUTTON), "Could not load checkout button"+NConstants.CART_CHECKOUT_BUTTON);
 		cartCheckoutButton.click();
-		
+
 		wait = new WebDriverWait(aDriver, 20);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(NConstants.PICK_IMAGE)));
 		Assert.assertTrue(isElementPresent(NConstants.PICK_IMAGE), "Could not load pick impage"+NConstants.PICK_IMAGE);
-		
+
 		wait = new WebDriverWait(aDriver, 20);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(NConstants.PICK_LOCATION)));
 		Assert.assertTrue(isElementPresent(NConstants.PICK_LOCATION), "Could not load pick location"+NConstants.PICK_LOCATION);
 		//pickLocation.click();
-		
+
 		//wait = new WebDriverWait(aDriver, 20);
 		//wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(NConstants.CHECKOUT_ADDRESS)));
 		//Assert.assertTrue(isElementPresent(NConstants.CHECKOUT_ADDRESS), "Could not load pick location"+NConstants.CHECKOUT_ADDRESS);
 		//checkoutAddress.sendKeys("Downtown Dubai - Dubai, United Arab Emirates - Downtown Dubai - Dubai - United Arab Emirates");
 		//checkoutAddress.click();
-		
+
 		//Assert.assertTrue(isElementPresent(NConstants.CHECKOUT_ADDRESS1), "Could not load address1 from map"+NConstants.CHECKOUT_ADDRESS1);
 		//checkoutAddress1.sendKeys("Downtown Dubai - Dubai, United Arab Emirates - Downtown Dubai - Dubai - United Arab Emirates");
 		//checkoutAddress1.click();
@@ -297,8 +355,8 @@ public class CheckoutPage extends BasePage {
 		//Assert.assertTrue(isElementPresent(NConstants.CHECKOUT_ADDRESS2), "Could not load address2 from map"+NConstants.CHECKOUT_ADDRESS2);
 		//checkoutAddress2.sendKeys("Downtown Dubai - Dubai, United Arab Emirates - Downtown Dubai - Dubai - United Arab Emirates");
 		//checkoutAddress2.click();
-		
-		
+
+
 		wait = new WebDriverWait(aDriver, 20);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(NConstants.CHECKOUT_ADDRESS_TYPE)));
 		Assert.assertTrue(isElementPresent(NConstants.CHECKOUT_ADDRESS_TYPE), "Could not select address Spinner button"+NConstants.CHECKOUT_ADDRESS_TYPE);
@@ -311,43 +369,43 @@ public class CheckoutPage extends BasePage {
 				break;
 			}
 		}
-		
+
 		wait = new WebDriverWait(aDriver, 40);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(NConstants.CHECKOUT_ADDRESS_NAME)));
 		Assert.assertTrue(isElementPresent(NConstants.CHECKOUT_ADDRESS_NAME), "Could not load checkout address"+NConstants.CHECKOUT_ADDRESS_NAME);
 		checkoutAddressName.sendKeys("Home");
 		aDriver.hideKeyboard();
-		
+
 		wait = new WebDriverWait(aDriver, 40);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(NConstants.CHECKOUT_PERSON_NAME)));
 		Assert.assertTrue(isElementPresent(NConstants.CHECKOUT_PERSON_NAME),"Could not load checkout person name field"+NConstants.CHECKOUT_PERSON_NAME);
 		checkoutPersonName.sendKeys("Mr. Kartik");
 		aDriver.hideKeyboard();
-		
+
 		wait = new WebDriverWait(aDriver, 40);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(NConstants.CHECKOUT_PERSON_MOBILE)));
 		Assert.assertTrue(isElementPresent(NConstants.CHECKOUT_PERSON_MOBILE),"Could not load checkout person mobile field"+NConstants.CHECKOUT_PERSON_MOBILE);
 		checkoutPersonMobile.sendKeys("0566681264");
 		aDriver.hideKeyboard();
-		
+
 		wait = new WebDriverWait(aDriver, 40);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(NConstants.CHECKOUT_PERSON_APARTMENT)));
 		Assert.assertTrue(isElementPresent(NConstants.CHECKOUT_PERSON_APARTMENT),"Could not load checkout person apartment number field"+NConstants.CHECKOUT_PERSON_APARTMENT);
 		checkoutPersonApartment.sendKeys("3927");
 		aDriver.hideKeyboard();
-		
+
 		wait = new WebDriverWait(aDriver, 40);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(NConstants.CHECKOUT_PERSON_FLOOR)));
 		Assert.assertTrue(isElementPresent(NConstants.CHECKOUT_PERSON_FLOOR),"Could not load checkout person floor number field"+NConstants.CHECKOUT_PERSON_FLOOR);
 		checkoutPersonFloor.sendKeys("39");
 		aDriver.hideKeyboard();
-		
+
 		wait = new WebDriverWait(aDriver, 40);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(NConstants.CHECKOUT_PERSON_BUILDING)));
 		Assert.assertTrue(isElementPresent(NConstants.CHECKOUT_PERSON_BUILDING),"Could not load checkout person building number field"+NConstants.CHECKOUT_PERSON_BUILDING);
 		checkoutPersonBuilding.sendKeys("3");
 		aDriver.hideKeyboard();
-		
+
 		wait = new WebDriverWait(aDriver, 40);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(NConstants.CHECKOUT_PERSON_AREA_NAME)));
 		Assert.assertTrue(isElementPresent(NConstants.CHECKOUT_PERSON_AREA_NAME),"Could not load checkout person area name"+NConstants.CHECKOUT_PERSON_AREA_NAME);
@@ -355,7 +413,7 @@ public class CheckoutPage extends BasePage {
 		//wait = new WebDriverWait(aDriver, 40);
 		//checkoutPersonAreaName.sendKeys("Downtown Dubai");
 		//aDriver.hideKeyboard();
-		
+
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(NConstants.CHECKOUT_PERSON_LANDMARK)));
 		Assert.assertTrue(isElementPresent(NConstants.CHECKOUT_PERSON_LANDMARK),"Could not load checkout person landmark name field"+NConstants.CHECKOUT_PERSON_LANDMARK);
 		checkoutPersonLandmark.sendKeys("Burj Al-Arab");
@@ -366,26 +424,26 @@ public class CheckoutPage extends BasePage {
 		Double screenHeightEnd = dimensions.getHeight() * 0.2;
 		int scrollEnd = screenHeightEnd.intValue();
 		aDriver.swipe(0,scrollStart,0,scrollEnd,2000);
-		
+
 		wait = new WebDriverWait(aDriver, 40);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(NConstants.CHECKOUT_PERSON_CITY)));
 		Assert.assertTrue(isElementPresent(NConstants.CHECKOUT_PERSON_CITY),"Could not load checkout person city name field"+NConstants.CHECKOUT_PERSON_CITY);
 		checkoutPersonCity.sendKeys("Dubai");
 		aDriver.hideKeyboard();
-		
+
 		wait = new WebDriverWait(aDriver, 40);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(NConstants.CHECKOUT_PERSON_COUNTRY)));
 		Assert.assertTrue(isElementPresent(NConstants.CHECKOUT_PERSON_COUNTRY),"Could not load checkout person country name field"+NConstants.CHECKOUT_PERSON_COUNTRY);
-		
+
 		//checkoutPersonCountry.sendKeys("United Arab Emirates");
 		//aDriver.hideKeyboard();
-		
+
 		wait = new WebDriverWait(aDriver, 100);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(NConstants.CHECKOUT_SAVE_ADDRESS)));
 		Assert.assertTrue(isElementPresent(NConstants.CHECKOUT_SAVE_ADDRESS),"Could not load checkout person save address field"+NConstants.CHECKOUT_SAVE_ADDRESS);
 		checkoutSaveAddress.click();
 		//aDriver.pressKeyCode(66);
-		
+
 		wait = new WebDriverWait(aDriver, 40);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(NConstants.PLACE_ORDER)));
 		Assert.assertTrue(isElementPresent(NConstants.PLACE_ORDER),"Could not load place order field"+NConstants.PLACE_ORDER);
@@ -433,7 +491,7 @@ public class CheckoutPage extends BasePage {
 		Assert.assertTrue(isElementPresent(NConstants.SHIP_GRAND_TOTAL_VALUE),"Could not load shipping item grand total value"+NConstants.SHIP_GRAND_TOTAL_VALUE);
 		Assert.assertTrue(isElementPresent(NConstants.SHIP_CONTINUE_BUTTON),"Could not load continue button"+NConstants.SHIP_CONTINUE_BUTTON);
 		shipContinueButton.click();
-		
+
 		Assert.assertTrue(isElementPresent(NConstants.PAYMENT_DETAILS),"Could not load payment details label"+NConstants.PAYMENT_DETAILS);
 		Assert.assertTrue(isElementPresent(NConstants.PAYMENT_CARD_TITLE),"Could not load credit card title"+NConstants.PAYMENT_CARD_TITLE);
 		Assert.assertTrue(isElementPresent(NConstants.PAYMENT_AMOUNT),"Could not load amount text"+NConstants.PAYMENT_AMOUNT);
@@ -476,26 +534,9 @@ public class CheckoutPage extends BasePage {
 		Assert.assertTrue(isElementPresent(NConstants.FOOTER_LOGO),"Could not load footer logo"+NConstants.FOOTER_LOGO);
 		Assert.assertTrue(isElementPresent(NConstants.PAY_BUTTON),"Could not load pay button"+NConstants.PAY_BUTTON);
 		payButton.click();
+		}
+		*/
 
-
-		
-		
-
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
 	}
-			*/
+
 }
