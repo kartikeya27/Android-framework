94c2360f60ff5b9ab8d1df09dc4ef7c84b7a9052
apopa <apopa@noon.com>
2017-03-20 11:11:47 +0400

    excluding some tests plus small fixes

--- src/test/java/com/noon/mobileapp/ApplyFilterTest.java
+++ src/test/java/com/noon/mobileapp/ApplyFilterTest.java
@@ -1,5 +1,6 @@
 package com.noon.mobileapp;
 
+import org.junit.Ignore;
 import org.testng.SkipException;
 import org.testng.annotations.Test;
 import com.noon.mobileapp.base.BaseTest;
@@ -12,7 +13,7 @@ public class ApplyFilterTest extends BaseTest {
 	
 	String testName="ApplyFilterTest";
 	
-	@Test
+	@Ignore
 	public void appyFilterTest() {
 		test = rep.startTest(testName);
 		test.log(LogStatus.INFO, "Starting the test");
